a3183e5e2a3d80d6e08727c97c5bc51e4618ab4b
Chelik <geeneelair@gmail.com>
2022-09-25 18:21:11 +0300

    added 3 more functions: 1)check_status - to check amount of messages 2)send_message - prototype of sending a message function 3)get_message - prototype of getting message function

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+list_of_messages = []
+
 
 @app.route('/')
 def hello():
-    return 'hello, world'
+    return 'Messanger Flask Server is running! ' \
+            '<br> <a href="/status">Check status</a>'
+
+
+@app.route('/status')
+def check_status():
+    return {
+        'messages_count': len(list_of_messages)
+    }
+
+
+@app.route('/api/Messanger', methods=['POST'])
+def send_message():
+    msg = request.json
+    print(msg)
+    #
+    list_of_messages.append(msg)
+    print(msg)
+    msg_text = f'{msg["UserName"]} <{msg["TimeStamp"]}>: {msg["MessageText"]}'
+    print(f'Всего сообщений: {len(list_of_messages)} Посланное сообщение: {msg_text}')
+    return f'Сообщение успешно отправлено. Всего сообщений: {len(list_of_messages)}', 200
+
+
+@app.route('/api/Messanger/<int:id>')
+def get_message(id):
+    print(id)
+    if id >= 0 and id < len(list_of_messages):
+        print(list_of_messages[id])
+        return list_of_messages[id], 200
+    else:
+        return 'Not Found', 400
 
 if __name__ == '__main__':
     app.run()
